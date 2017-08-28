ec9a34623ee52a752b5cd67c2c83af1aaaf44960
Anders Liu <anders.liu@gmail.com>
2017-08-28 16:42:59 -0700

    Add draw text with scale example.

---
+++ AL_ILI9341/examples/DrawTextWithScale/DrawTextWithScale.ino
@@ -0,0 +1,40 @@
+#include "AL_ILI9341.h"
+#include "AL_Font.h"
+
+// Wiring
+#define TFT_PORT PORTF
+#define TFT_PIN PINF
+#define TFT_DDR DDRF
+#define TFT_RST A12
+#define TFT_CS A11
+#define TFT_RS A10
+#define TFT_WR A9
+#define TFT_RD A8
+
+AL_ILI9341 tft(
+    &TFT_PORT, &TFT_PIN, &TFT_DDR,
+    TFT_RST, TFT_CS, TFT_RS, TFT_WR, TFT_RD);
+
+AL_RgbColor backColor{255, 255, 255};
+AL_RgbColor text1Color{255, 0, 0};
+AL_RgbColor text2Color{0, 255, 0};
+AL_RgbColor text3Color{0, 0, 255};
+AL_RgbColor text4Color{0, 255, 255};
+AL_RgbColor text5Color{255, 255, 0};
+
+void setup()
+{
+    tft.setup();
+    tft.setOrientation(AL_SO_LANDSCAPE2);
+    tft.fillRect(0, 0, tft.getWidth(), tft.getHeight(), backColor);
+
+    tft.drawText(10, 10, text1Color, backColor, 1, "hello");
+    tft.drawText(10, 26, text2Color, backColor, 2, "hello");
+    tft.drawText(10, 58, text3Color, backColor, 3, "hello");
+    tft.drawText(10, 106, text4Color, backColor, 4, "hello");
+    tft.drawText(10, 170, text5Color, backColor, 5, "hello");
+}
+
+void loop()
+{
+}
